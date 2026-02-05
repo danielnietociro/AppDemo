ccd424227529ba8b31de8acf6f585aff3d0c356c
Juan Daniel Nieto <daniel.nieto.ciro@gmail.com>
2026-02-05 16:59:00 -0500

    Create basic app to functions: add, modify and delete

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -8,6 +8,35 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
+  newTask: string = '';
+  tasks: { text: string; completed: boolean; editing: boolean }[] = [];
+
   constructor() {}
+
+  addTask() {
+    if (this.newTask.trim()) {
+      this.tasks.push({ text: this.newTask, completed: false, editing: false });
+      this.newTask = '';
+    }
+  }
+
+  toggleTask(index: number) {
+    this.tasks[index].completed = !this.tasks[index].completed;
+  }
+
+  deleteTask(index: number) {
+    this.tasks.splice(index, 1);
+  }
+
+  editTask(index: number) {
+    this.tasks[index].editing = !this.tasks[index].editing;
+  }
+
+  saveTask(index: number, newText: string) {
+    if (newText.trim()) {
+      this.tasks[index].text = newText.trim();
+    }
+    this.tasks[index].editing = false;
+  }
 
 }
--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -4,31 +4,18 @@ import { TabsPage } from './tabs.page';
 
 const routes: Routes = [
   {
-    path: 'tabs',
+    path: 'task',
     component: TabsPage,
     children: [
       {
         path: 'tab1',
         loadChildren: () => import('../tab1/tab1.module').then(m => m.Tab1PageModule)
-      },
-      {
-        path: 'tab2',
-        loadChildren: () => import('../tab2/tab2.module').then(m => m.Tab2PageModule)
-      },
-      {
-        path: 'tab3',
-        loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
-      },
-      {
-        path: '',
-        redirectTo: '/tabs/tab1',
-        pathMatch: 'full'
       }
     ]
   },
-  {
+  { 
     path: '',
-    redirectTo: '/tabs/tab1',
+    redirectTo: '/task/tab1',
     pathMatch: 'full'
   }
 ];
